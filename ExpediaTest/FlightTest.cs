9f6f5d762b54bdd31650047c2b469bdf80e33fc6
Arcessimus <dsfreak17@gmail.com>
2015-03-20 13:56:06 -0400

    Task 7

--- ExpediaTest/FlightTest.cs
+++ ExpediaTest/FlightTest.cs
@@ -4,14 +4,45 @@ using NUnit.Framework;
 
 namespace ExpediaTest
 {
-	[TestFixture()]
-	public class FlightTest
-	{
-		//TODO Task 7 items go here
+    [TestFixture()]
+    public class FlightTest
+    {
+        //TODO Task 7 items go here
         [Test()]
         public void TestFlightInit()
         {
-            var target = new Flight()
+            var target = new Flight(new DateTime(2015, 3, 21), new DateTime(2015, 3, 31), 100);
+            Assert.IsNotNull(target);
         }
-	}
+
+        [Test()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestThatFlightForcesStartDateToBeBeforeEndDate()
+        {
+            new Flight(new DateTime(2015, 3, 31), new DateTime(2015, 3, 21), 100);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatFlightMilesMustBePositive()
+        {
+            new Flight(new DateTime(2015, 3, 21), new DateTime(2015, 3, 31), -5);
+        }
+
+        [Test()]
+        public void TestCorrectBasePriceForOneDay()
+        {
+            var target = new Flight(new DateTime(2015, 3, 20), new DateTime(2015, 3, 21), 100);
+            Assert.AreEqual(220, target.getBasePrice());
+        }
+
+        [Test()]
+        public void TestCorrectBasePriceForFiveDays()
+        {
+            var target = new Flight(new DateTime(2015, 4, 1), new DateTime(2015, 4, 6), 100);
+            Assert.AreEqual(300, target.getBasePrice());
+        }
+
+    }
+
 }
